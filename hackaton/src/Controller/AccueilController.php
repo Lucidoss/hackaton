e280dbf978090132a123ee5d94c1127b42a4c23a
Martin <martin@email.com>
2022-10-21 15:55:17 +0200

    ajout de la page inscription et connexion

--- hackaton/src/Controller/AccueilController.php
+++ hackaton/src/Controller/AccueilController.php
@@ -15,4 +15,28 @@ class AccueilController extends AbstractController
             'controller_name' => 'AccueilController',
         ]);
     }
+
+    #[Route('/hackathon', name: 'app_hackathon')]
+    public function hackathon(): Response
+    {
+        return $this->render('accueil/hackathon.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    #[Route('/connexion', name: 'app_connexion')]
+    public function connexion(): Response
+    {
+        return $this->render('accueil/connexion.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    #[Route('/inscription', name: 'app_inscription')]
+    public function inscription(): Response
+    {
+        return $this->render('accueil/inscription.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
 }
