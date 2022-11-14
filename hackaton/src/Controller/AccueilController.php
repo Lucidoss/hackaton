53f9ecd32832d76db842fb93fbebf7785f13ce42
Remery <romain.dugas85@gmail.com>
2022-11-14 15:37:38 +0100

    a

--- hackaton/src/Controller/AccueilController.php
+++ hackaton/src/Controller/AccueilController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Participant;
-use Symfony\Component\BrowserKit\Request;
 
 class AccueilController extends AbstractController
 {
