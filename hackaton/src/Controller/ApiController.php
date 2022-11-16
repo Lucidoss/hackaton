cbe50415763ae30dbc96e73dd875c0068988f251
Remery <romain.dugas85@gmail.com>
2022-11-16 09:19:49 +0100

    Création de l'API pour récupérer les hackathons

---
+++ hackaton/src/Controller/ApiController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Hackathon;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api/hackathons', name: 'app_api_hackathons', methods: ['GET'])]
+    public function getListeHackathons(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Hackathon::class);
+        $lesHackathons = $repository->findAll();
+        $tableau= [];
+        foreach($lesHackathons as $unHackathon) {
+            $tableau[]=[
+                'id'=>$unHackathon->getId(),
+                'dateDebut'=>$unHackathon->getDATEDEBUT(),
+                'dateFin'=>$unHackathon->getDATEFIN(),
+                'heureDebut'=>$unHackathon->getHEUREDEBUT(),
+                'heureFin'=>$unHackathon->getHEUREFIN(),
+                'lieu'=>$unHackathon->getLIEU(),
+                'rue'=>$unHackathon->getRUE(),
+                'ville'=>$unHackathon->getVILLE(),
+                'cp'=>$unHackathon->getCP(),
+                'theme'=>$unHackathon->getTHEME(),
+                'description'=>$unHackathon->getDESCRIPTION(),
+                'image'=>$unHackathon->getIMAGE(),
+                'dateLimite'=>$unHackathon->getDATELIMITE(),
+                'nbPlaces'=>$unHackathon->getNBPLACES(),
+            ];
+        }
+        return new JsonResponse($tableau);
+    }
+
+    #[Route('/api/hackathons/{id}', name: 'app_api_hackathon_id', methods: ['GET'])]
+    public function getLeHackathon(ManagerRegistry $doctrine, $id): Response
+    {
+        $repository = $doctrine->getRepository(Hackathon::class);
+        $leHackathon = $repository->find($id);
+        if ($leHackathon !== null) {
+            $tableau = [
+                'id'=>$leHackathon->getId(),
+                'dateDebut'=>$leHackathon->getDATEDEBUT(),
+                'dateFin'=>$leHackathon->getDATEFIN(),
+                'heureDebut'=>$leHackathon->getHEUREDEBUT(),
+                'heureFin'=>$leHackathon->getHEUREFIN(),
+                'lieu'=>$leHackathon->getLIEU(),
+                'rue'=>$leHackathon->getRUE(),
+                'ville'=>$leHackathon->getVILLE(),
+                'cp'=>$leHackathon->getCP(),
+                'theme'=>$leHackathon->getTHEME(),
+                'description'=>$leHackathon->getDESCRIPTION(),
+                'image'=>$leHackathon->getIMAGE(),
+                'dateLimite'=>$leHackathon->getDATELIMITE(),
+                'nbPlaces'=>$leHackathon->getNBPLACES(),
+            ];
+            return new JsonResponse($tableau);
+        } else {
+            return new JsonResponse(['message' => 'Hackathon not found'], Response::HTTP_NOT_FOUND);
+        }
+    }
+}
